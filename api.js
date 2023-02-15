2b20cb761bf72e72556e226d8ef77a8805733de9
pedrovitor <pedro.vitor@opovodigital.com>
2023-02-15 19:43:30 -0300

    first commit

---
+++ api.js
@@ -0,0 +1,6 @@
+// Substitua o CHAVE_DA_API com a chave gerada no site.
+
+export const API_KEY = 'api_key=b3b8ce82715f55196192ed4d13e2d015';
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+export const language = 'language=pt-BR';
---
+++ main.js
@@ -0,0 +1,45 @@
+import { API_KEY, BASE_URL, IMG_URL, language } from "./api.js";
+
+// const url = `${BASE_URL}02?${API_KEY}`
+
+// console.log(API_KEY, BASE_URL)
+
+// const  getMovie = async (url) => {
+//   const res = await fetch(url);
+//   const data = await res.json();
+//   console.log(data)
+// }
+// getMovie(url)
+
+const movie = {
+  id: 5000,
+  img: ``,
+  title: ``,
+  description: ``,
+};
+
+const getMovie = async () => {
+  //URL COMPLETA (base, id do filme, chave api e idioma)
+
+  const url = `${BASE_URL}${movie.id}?${API_KEY}&${language}`;
+
+
+  const res = await fetch(url);
+  console.log(res)
+  //JSON COM OS DADOS QUE VEM DA API
+  const data = await res.json();
+
+  console.log(url);
+  console.log(data);
+
+  movie.img = `${IMG_URL}${data.poster_path}`;
+  movie.title = data.title;
+  movie.description = data.overview;
+
+  document.querySelector("#img-movie").setAttribute("src", movie.img);
+  document.querySelector("#title-movie").innerHTML = movie.title;
+  document.querySelector("#overview-movie").innerHTML = movie.description;
+};
+
+getMovie();
+
