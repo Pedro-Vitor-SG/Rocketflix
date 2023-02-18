eca67c0a382196a87281dd27c2f607503d97a7b3
pedrovitor <pedro.vitor@opovodigital.com>
2023-02-18 16:53:11 -0300

    pegando id aleatorio, responvidade e layout de filme undefined

--- main.js
+++ main.js
@@ -1,31 +1,21 @@
 import { API_KEY, BASE_URL, IMG_URL, language } from "./api.js";
-
-// const url = `${BASE_URL}02?${API_KEY}`
-
-// console.log(API_KEY, BASE_URL)
-
-// const  getMovie = async (url) => {
-//   const res = await fetch(url);
-//   const data = await res.json();
-//   console.log(data)
-// }
-// getMovie(url)
-
 const movie = {
-  id: 1858,
+  id: 1958,
   img: ``,
   title: ``,
   description: ``,
 };
 
 const getMovie = async () => {
+  const random = Math.floor(Math.random() * 3000);
+  console.log(random);
+  movie.id = random;
   //URL COMPLETA (base, id do filme, chave api e idioma)
 
   const url = `${BASE_URL}${movie.id}?${API_KEY}&${language}`;
 
-
   const res = await fetch(url);
-  console.log(res)
+  console.log(res);
   //JSON COM OS DADOS QUE VEM DA API
   const data = await res.json();
 
@@ -36,12 +26,34 @@ const getMovie = async () => {
   movie.title = data.title;
   movie.description = data.overview;
 
-  document.querySelector("#img-movie").setAttribute("src", movie.img);
-  document.querySelector("#title-movie").innerHTML = movie.title;
-  document.querySelector("#overview-movie").innerHTML = movie.description;
+  if (data.success === false) {
+    console.log("Não achou o filme");
+    document.querySelector("#view-movie").classList.add("not-found");
+    document
+      .querySelector("#img-movie")
+      .setAttribute("src", "/assets/not-found-movie.png");
+    document
+      .querySelector("#img-movie")
+      .setAttribute("alt", `Poster para filme não encontrado`);
+    document.querySelector("#title-movie").innerHTML =
+      "Ops, hoje não é dia de assistir filme. Bora codar! 🚀";
 
-  document.querySelector('#view-movie').style.display = 'block'
+    document.querySelector("#overview-movie").style.display = "none";
+  } else {
+    console.log("achou");
+    document.querySelector("#view-movie").classList.remove("not-found");
+
+    document.querySelector("#img-movie").setAttribute("src", movie.img);
+    document
+      .querySelector("#img-movie")
+      .setAttribute("alt", `Poster do Filme: ${movie.title}`);
+    document.querySelector("#title-movie").innerHTML = movie.title;
+    document.querySelector("#overview-movie").style.display = "block";
+    document.querySelector("#overview-movie").innerHTML = movie.description;
+    document.querySelector("#overview-movie").style.display = '-webkit-box';
+
+    document.querySelector("#view-movie").style.display = "flex";
+  }
 };
 
-document.querySelector('#btn-query').addEventListener('click', getMovie)
-
+document.querySelector("#btn-query").addEventListener("click", getMovie);
